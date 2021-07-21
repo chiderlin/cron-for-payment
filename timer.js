0fe2c6d067116c0c4b7034630f203200ba7d7634
chiderlin <chiderlin36@gmail.com>
2021-07-21 11:52:05 +0800

    clean & find plan issue,add try catch

--- timer.js
+++ timer.js
@@ -17,23 +17,13 @@ const cronjob = schedule.scheduleJob('0 0 10 * * ?',()=>{ // 每天10:00執行�
     format_today = today.format('YYYY-MM-DD');
     next_pay_date = today.add(1, 'days').format('YYYY-MM-DD'); // 測試先加一天
     console.log(format_today);
-    // const db = mysql.createConnection({
-    //     host: process.env.DB_HOST,
-    //     user: process.env.DB_USER,
-    //     password: process.env.DB_PWD,
-    //     database: process.env.DB,
-    //     dateStrings: true,
-    // });
-    // db.connect()
-    // console.log('Connected!');
+
     const sql = `select * from Payments where next_pay_date = "${format_today}"`;
     query(sql, (err,res,field)=>{
         if(err){
             throw err;
         } else {
             for(let i=0; i<res.length; i++) {
-                // console.log("card_key:",res[i].card_key)
-                // console.log(res[i].card_token)
                 const paymentId = res[i].id;
                 const card_key = res[i].card_key
                 const card_token = res[i].card_token;
@@ -44,7 +34,9 @@ const cronjob = schedule.scheduleJob('0 0 10 * * ?',()=>{ // 每天10:00執行�
                     if(err){
                         throw err;
                     } else {
-                        plan = res[0].plan;
+                        console.log(paymentId);
+                        console.log(res)
+                        plan = res[0].plan; //[ TextRow { plan: 1000 } ]
                     }
                     const post_data = {
                         'card_key':card_key,
@@ -60,39 +52,6 @@ const cronjob = schedule.scheduleJob('0 0 10 * * ?',()=>{ // 每天10:00執行�
             }
         }
     })
-    // db.query(sql,(err,res)=>{
-    //     if(err){
-    //         throw err;
-    //     } else {
-    //         for(let i=0; i<res.length; i++) {
-    //             // console.log("card_key:",res[i].card_key)
-    //             // console.log(res[i].card_token)
-    //             const paymentId = res[i].id;
-    //             const card_key = res[i].card_key
-    //             const card_token = res[i].card_token;
-    //             const userId = res[i].UserId;
-    //             let plan;
-    //             const sql = `select plan from Users where id=${userId}`;
-    //             db.query(sql,(err,res)=>{
-    //                 if(err){
-    //                     throw err;
-    //                 } else {
-    //                     plan = res[0].plan;
-    //                 }
-    //                 const post_data = {
-    //                     'card_key':card_key,
-    //                     'card_token':card_token,
-    //                     'partner_key': 'partner_PyJKIbMCqgsYpYiouacHI67J0jT0xOdGBGSO9e05OdiB1RHhYSDdjioD',
-    //                     'merchant_id': 'chi_CTBC',
-    //                     'currency': 'TWD',
-    //                     'amount': plan,
-    //                     'details': 'pay gym service monthly',
-    //                 }
-    //                 payByToken(post_data,paymentId);
-    //             })
-    //         }
-    //     }
-    // })
 });
 
 
@@ -114,15 +73,8 @@ function payByToken(post_data,paymentId){
             const rec_trade_id = data.rec_trade_id;
             // 把其他資料存到order db
             insert_order(transaction_id,amount,currency,rec_trade_id, paymentId, (ok)=>{
+                
                 // 更新付款時間到payments => next pay date
-                // const db = mysql.createConnection({
-                //     host: process.env.DB_HOST,
-                //     user: process.env.DB_USER,
-                //     password: process.env.DB_PWD,
-                //     database: process.env.DB,
-                //     dateStrings: true,
-                // });
-                // db.connect()
                 const sql = `update Payments set next_pay_date = "${next_pay_date}" where next_pay_date = "${format_today}"`
                 query(sql,(err,res,field)=>{
                     if(err) {
@@ -131,28 +83,13 @@ function payByToken(post_data,paymentId){
                     count++;
                     console.log(count + 'record(s) updated');
                 })
-                // db.query(sql, (err,result)=>{
-                //     if(err) {
-                //         throw err;
-                //     }
-                //     count++;
-                //     console.log(count + 'record(s) updated');
-                //     db.end();
-                // })
             })    
         }
     })
 };
 
 function insert_order(transaction_id,amount,currency,rec_trade_id, paymentId, callback){
-    // const db = mysql.createConnection({
-    //     host: process.env.DB_HOST,
-    //     user: process.env.DB_USER,
-    //     password: process.env.DB_PWD,
-    //     database: process.env.DB,
-    //     dateStrings: true,
-    // });
-    // db.connect()
+
     const sql = `insert into Orders(transaction_id, currency, amount,rec_trade_id, paymentId) value ('${transaction_id}','${currency}','${amount}','${rec_trade_id}','${paymentId}')`
     query(sql,(err,res,field)=>{
         if(err){
@@ -161,11 +98,4 @@ function insert_order(transaction_id,amount,currency,rec_trade_id, paymentId, ca
             return callback('insert 1 record');
         }
     })
-    // db.query(sql, (err, result)=>{
-    //     if(err){
-    //         throw err;
-    //     } else {
-    //         return callback('insert 1 record');
-    //     }
-    // })
 }
